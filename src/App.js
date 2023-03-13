1b11cbb4ad120798f45687910726c26e8be45a4e
Munyaradzi <munyargumbu@gmail.com>
2023-03-13 13:29:23 +0200

    new footer changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 
 import { Landing } from "./pages";
+import { Footer } from "./components";
 
 const App = () => (
   <div className="app-container">
     <Landing />
+    <Footer />
   </div>
 );
 
--- src/pages/LandingPage/Landing.js
+++ src/pages/LandingPage/Landing.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Landing.css";
 import { Navbar } from "../../components";
+import { Footer } from "../../components";
 
 const Landing = () => (
   <div className="landing-container">
     <Navbar />
+    <div className = 'Landing-text'>
     <h1>LANDING</h1>
+    </div>
+    <Footer />
+
   </div>
 );
 
